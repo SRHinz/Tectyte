e73e1119700044b4cb95be3cdb6f04076506132c
SRhinz <srhinz@coe.edu>
2020-09-16 12:00:02 -0500

    Added in code for creating a dictionary of accounts, with keys as a username-password tuple

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -9,13 +9,14 @@ namespace ConsoleTest
         static void Main(string[] args)
         {
 			CourseDatabase CourseData = new CourseDatabase();
+			UserDatabase UserData = new UserDatabase();
             Console.WriteLine("Hello World!");
         }
     }
 
 	public class UserDatabase
 	{
-		Dictionary<string, (string, string, string, string, string, string, string[])> uDatabase = new Dictionary<string, (string, string, string, string, string, string, string[])>();
+		Dictionary<(string, string), Account> uDatabase = new Dictionary<(string, string), Account>();
 		public UserDatabase()
 		{
 			Console.WriteLine("Enter location of UserDatabase input file:");
@@ -24,7 +25,9 @@ namespace ConsoleTest
 			System.IO.StreamReader file = new System.IO.StreamReader(@loc);
 			while ((line = file.ReadLine()) != null)
 			{
-				System.Console.WriteLine(line);
+				string[] words = line.Split(' ');
+				uDatabase.Add((words[0], words[1]), new Account(words));
+
 			}
 			file.Close();
 
@@ -37,8 +40,25 @@ namespace ConsoleTest
 		private string status;
 		private string fName, mName, lName;
 
+		public Account(string[] args)
+        {
+			UserName = args[0];
+			Password = args[1];
+			FName = args[2];
+			MName = args[3];
+			LName = args[4];
+			Status = args[5];
 
-	}
+			
+        }
+
+        public string UserName { get => userName; set => userName = value; }
+        public string Password { get => password; set => password = value; }
+        public string Status { get => status; set => status = value; }
+        public string FName { get => fName; set => fName = value; }
+        public string MName { get => mName; set => mName = value; }
+        public string LName { get => lName; set => lName = value; }
+    }
 
 	public class CourseDatabase
 	{
