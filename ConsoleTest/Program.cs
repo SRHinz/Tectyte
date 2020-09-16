c81f561114d5accd00951ab1ac9d90a8af92b981
SRhinz <srhinz@coe.edu>
2020-09-16 13:19:02 -0500

    Made it possible to display all the contents of the user database. this should serve as a skeleton for being able to display the course list.

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Linq;
 
 namespace ConsoleTest
 {
@@ -8,30 +10,57 @@ namespace ConsoleTest
     {
         static void Main(string[] args)
         {
-			CourseDatabase CourseData = new CourseDatabase();
+			//CourseDatabase CourseData = new CourseDatabase();
 			UserDatabase UserData = new UserDatabase();
+			UserData.viewDatabase();
             Console.WriteLine("Hello World!");
         }
     }
 
 	public class UserDatabase
 	{
-		Dictionary<(string, string), Account> uDatabase = new Dictionary<(string, string), Account>();
+		private Dictionary<(string, string), Account> uDatabase = new Dictionary<(string, string), Account>(); //Dictionary of Accounts, with username password tuple as the key
 		public UserDatabase()
 		{
+			int[] dataBasecounter = {10, 10, 15, 15, 15, 10};
 			Console.WriteLine("Enter location of UserDatabase input file:");
 			string loc = Console.ReadLine();
 			string line;
 			System.IO.StreamReader file = new System.IO.StreamReader(@loc);
 			while ((line = file.ReadLine()) != null)
 			{
-				string[] words = line.Split(' ');
+				string[] words = new string[6];
+				int a = 0;
+				for (int i = 0; i <= 5; i+=1)
+                {
+					string subString = "";
+					if (i!=5)					//This if else statement handles the status portion of the input line, so that an out of range exception is not thrown for the SubString
+					{
+						subString = line.Substring(a, dataBasecounter[i]).TrimEnd(); 
+					}
+					else
+                    {
+						subString = line.Substring(a, line.Length - a); //If it is the last bit of the line, then it simply checks how many characters there are between the end of the line and where it is starting from, and gives that as the length to increment.
+                    }
+					
+					a = a + dataBasecounter[i] + 1;
+					words[i] = subString;
+                }
 				uDatabase.Add((words[0], words[1]), new Account(words));
 
 			}
 			file.Close();
-
 		}
+		public void viewDatabase()
+        {
+			Console.WriteLine("User Database");
+			foreach (KeyValuePair<(string, string), Account> user in uDatabase)
+            {
+				Console.WriteLine("Key: {0}, Value: {1}", user.Key, user.Value.getAccount());
+            }
+        }
+
+		
 	}
 
 	public class Account
@@ -48,10 +77,12 @@ namespace ConsoleTest
 			MName = args[3];
 			LName = args[4];
 			Status = args[5];
-
-			
         }
 
+		public string getAccount()
+        {
+			return (UserName + ' ' + Password + ' ' + FName + ' ' + MName + ' ' + LName + ' ' + Status);
+        }
         public string UserName { get => userName; set => userName = value; }
         public string Password { get => password; set => password = value; }
         public string Status { get => status; set => status = value; }
