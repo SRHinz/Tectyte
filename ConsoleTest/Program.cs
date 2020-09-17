f87cff24a90de56e49dc366975d55b21080be9c3
brkolpin <brkolpin@coe.edu>
2020-09-17 14:19:24 -0500

    Added Course Database Creator

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -11,8 +11,10 @@ namespace ConsoleTest
         static void Main(string[] args)
         {
 			//CourseDatabase CourseData = new CourseDatabase();
-			UserDatabase UserData = new UserDatabase();
-			UserData.viewDatabase();
+			//UserDatabase UserData = new UserDatabase();
+			CourseDatabase CourseData = new CourseDatabase();
+			CourseData.viewDatabase();
+			//UserData.viewDatabase();
             Console.WriteLine("Hello World!");
         }
     }
@@ -91,17 +93,59 @@ namespace ConsoleTest
 
 	public class CourseDatabase
 	{
+		private Dictionary<string, Course> cDatabase = new Dictionary<string, Course>();
 		public CourseDatabase()                             //This is the constructor for creating the database of courses
-		{ 
+		{
+			int[] stringLengths = { 10, 15, 10, 4, 3, 1, 5, 5 };
 			Console.WriteLine("Enter location of CourseDatabase input file:");
 			string loc = Console.ReadLine();
 			string line;
 			System.IO.StreamReader file = new System.IO.StreamReader(@loc);
 			while ((line = file.ReadLine()) != null)
 			{
-				System.Console.WriteLine(line);
+				string[] inputs = new string[8];
+				int start = 0;
+				for (int i = 0; i <= 7; i++)
+                {
+					string subString = "";
+					if (i == 5) // takes care of classes with more than one time block
+                    {
+						subString = line.Substring(start, stringLengths[i]).TrimEnd();
+						if (subString == "1")
+						{
+							inputs[i] = subString;
+							start = start + stringLengths[i] + 1;
+							i++;
+							subString = line.Substring(start, stringLengths[i]).TrimEnd();
+							inputs[i] = subString;
+							break;
+						}
+
+
+					}
+					else if (i != 7)       //This if else statement handles the status portion of the input line, so that an out of range exception is not thrown for the SubString
+					{
+						subString = line.Substring(start, stringLengths[i]).TrimEnd();
+					}
+					else
+					{
+						subString = line.Substring(start, line.Length - start); //If it is the last bit of the line, then it simply checks how many characters there are between the end of the line and where it is starting from, and gives that as the length to increment.
+					}
+					inputs[i] = subString;
+					start = start + stringLengths[i] + 1;
+				}
+				cDatabase.Add(inputs[0], new Course(inputs));
 			}
 			file.Close();
+
+		}
+		public void viewDatabase()
+        {
+			Console.WriteLine("Course Database");
+			foreach (KeyValuePair<string, Course> user in cDatabase)
+            {
+				Console.WriteLine("Key: {0}, Value: {1}", user.Key, user.Value.getCourse());
+			}
 
 		}
 	}
@@ -109,8 +153,30 @@ namespace ConsoleTest
 	public class Course
 	{
 		private string courseName, courseTitle, instructor;
-		private int totatSeats, availableSeats;
+		private int totalSeats, availableSeats, numTimeBlocks;
 		private float Credits;
+		string[] timeBlocks = new string[2];
+
+		public Course(string[] args)
+        {
+			courseName = args[0];
+			courseTitle = args[1];
+			instructor = args[2];
+			Credits = float.Parse(args[3]);
+			totalSeats = int.Parse(args[4]);
+			numTimeBlocks = int.Parse(args[5]);
+			int s = 6;
+			for (int i = 0; i < numTimeBlocks; i++, s++)
+				timeBlocks[i] = args[s];
+
+			
+
+        }
+
+		public string getCourse()
+        {
+			return (courseTitle + ' ' + instructor + ' ' + Credits.ToString() + ' ' + totalSeats.ToString() + ' ' + numTimeBlocks.ToString() + ' ' + timeBlocks[0] + ' ' + timeBlocks[1]);
+        }
 	}
 
 
