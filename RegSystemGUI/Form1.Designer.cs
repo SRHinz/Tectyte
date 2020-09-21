d4dd2c9a54cf2a99f6252c968c31cadfe78dd570
SamHinz <srhin@coe.edu>
2020-09-21 15:05:09 -0500

    Moved everything to Form1.CS
    Going to try and make things all work on one form.

--- RegSystemGUI/Form1.Designer.cs
+++ RegSystemGUI/Form1.Designer.cs
@@ -37,60 +37,66 @@
             // 
             // UsernameInput
             // 
-            this.UsernameInput.Location = new System.Drawing.Point(274, 243);
+            this.UsernameInput.Location = new System.Drawing.Point(87, 85);
+            this.UsernameInput.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.UsernameInput.Multiline = true;
             this.UsernameInput.Name = "UsernameInput";
-            this.UsernameInput.Size = new System.Drawing.Size(379, 44);
+            this.UsernameInput.Size = new System.Drawing.Size(122, 18);
             this.UsernameInput.TabIndex = 0;
             this.UsernameInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // PasswordText
             // 
-            this.PasswordText.Location = new System.Drawing.Point(274, 344);
+            this.PasswordText.Location = new System.Drawing.Point(87, 121);
+            this.PasswordText.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.PasswordText.Name = "PasswordText";
             this.PasswordText.PasswordChar = '*';
-            this.PasswordText.Size = new System.Drawing.Size(379, 44);
+            this.PasswordText.Size = new System.Drawing.Size(122, 20);
             this.PasswordText.TabIndex = 1;
             this.PasswordText.UseSystemPasswordChar = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(387, 203);
+            this.label1.Location = new System.Drawing.Point(122, 71);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(164, 37);
+            this.label1.Size = new System.Drawing.Size(55, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Username";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(387, 304);
+            this.label2.Location = new System.Drawing.Point(122, 107);
+            this.label2.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(158, 37);
+            this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Password";
             this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(379, 434);
+            this.button2.Location = new System.Drawing.Point(120, 152);
+            this.button2.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(172, 54);
+            this.button2.Size = new System.Drawing.Size(54, 27);
             this.button2.TabIndex = 4;
             this.button2.Text = "Login";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(955, 654);
+            this.ClientSize = new System.Drawing.Size(302, 230);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.PasswordText);
             this.Controls.Add(this.UsernameInput);
+            this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
