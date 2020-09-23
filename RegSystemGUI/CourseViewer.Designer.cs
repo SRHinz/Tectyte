86ab409e0415084bc7b5742f28d09717dcf81594
misha-matchette <mgmatchette@coe.edu>
2020-09-23 15:02:01 -0500

    Changed CourseDataGrid background color

--- RegSystemGUI/CourseViewer.Designer.cs
+++ RegSystemGUI/CourseViewer.Designer.cs
@@ -35,7 +35,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1936, 27);
+            this.button1.Location = new System.Drawing.Point(1374, 56);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(333, 100);
             this.button1.TabIndex = 1;
@@ -50,13 +50,14 @@
             this.CourseDataGrid.AllowUserToResizeColumns = false;
             this.CourseDataGrid.AllowUserToResizeRows = false;
             this.CourseDataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.CourseDataGrid.BackgroundColor = System.Drawing.SystemColors.Control;
             this.CourseDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.CourseDataGrid.Location = new System.Drawing.Point(14, 56);
             this.CourseDataGrid.Name = "CourseDataGrid";
             this.CourseDataGrid.ReadOnly = true;
             this.CourseDataGrid.RowHeadersWidth = 123;
             this.CourseDataGrid.RowTemplate.Height = 46;
-            this.CourseDataGrid.Size = new System.Drawing.Size(1916, 818);
+            this.CourseDataGrid.Size = new System.Drawing.Size(1261, 818);
             this.CourseDataGrid.TabIndex = 3;
             this.CourseDataGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.CourseDataGrid_CellContentClick);
             // 
@@ -64,7 +65,7 @@
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(2282, 886);
+            this.ClientSize = new System.Drawing.Size(1806, 886);
             this.Controls.Add(this.CourseDataGrid);
             this.Controls.Add(this.button1);
             this.Name = "CourseViewer";
