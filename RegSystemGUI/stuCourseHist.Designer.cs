3afad32fb6f766c5fb8dab4abe4960185981eda6
misha-matchette <mgmatchette@coe.edu>
2020-10-02 14:04:18 -0500

    changed courseHis button color so i can test something

--- RegSystemGUI/stuCourseHist.Designer.cs
+++ RegSystemGUI/stuCourseHist.Designer.cs
@@ -37,13 +37,14 @@
             // 
             // MenuReturn
             // 
-            this.MenuReturn.Location = new System.Drawing.Point(833, 257);
-            this.MenuReturn.Margin = new System.Windows.Forms.Padding(1);
+            this.MenuReturn.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.MenuReturn.Location = new System.Drawing.Point(1250, 402);
+            this.MenuReturn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MenuReturn.Name = "MenuReturn";
-            this.MenuReturn.Size = new System.Drawing.Size(222, 42);
+            this.MenuReturn.Size = new System.Drawing.Size(333, 66);
             this.MenuReturn.TabIndex = 1;
             this.MenuReturn.Text = "Return to Menu";
-            this.MenuReturn.UseVisualStyleBackColor = true;
+            this.MenuReturn.UseVisualStyleBackColor = false;
             this.MenuReturn.Click += new System.EventHandler(this.MenuReturn_Click);
             // 
             // TermSelectorBox
@@ -53,9 +54,10 @@
             "Course History",
             "Current Courses",
             "Future Courses"});
-            this.TermSelectorBox.Location = new System.Drawing.Point(842, 34);
+            this.TermSelectorBox.Location = new System.Drawing.Point(1263, 53);
+            this.TermSelectorBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.TermSelectorBox.Name = "TermSelectorBox";
-            this.TermSelectorBox.Size = new System.Drawing.Size(121, 24);
+            this.TermSelectorBox.Size = new System.Drawing.Size(180, 33);
             this.TermSelectorBox.TabIndex = 2;
             this.TermSelectorBox.SelectedIndexChanged += new System.EventHandler(this.TermSelectorBox_SelectedIndexChanged);
             // 
@@ -68,24 +70,25 @@
             this.CourseDataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.CourseDataGrid.BackgroundColor = System.Drawing.SystemColors.Control;
             this.CourseDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.CourseDataGrid.Location = new System.Drawing.Point(34, 34);
+            this.CourseDataGrid.Location = new System.Drawing.Point(51, 53);
+            this.CourseDataGrid.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.CourseDataGrid.Name = "CourseDataGrid";
             this.CourseDataGrid.ReadOnly = true;
             this.CourseDataGrid.RowHeadersWidth = 30;
             this.CourseDataGrid.RowTemplate.Height = 46;
-            this.CourseDataGrid.Size = new System.Drawing.Size(731, 582);
+            this.CourseDataGrid.Size = new System.Drawing.Size(1096, 909);
             this.CourseDataGrid.TabIndex = 4;
             // 
             // stuCourseHist
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1180, 728);
+            this.ClientSize = new System.Drawing.Size(1770, 1138);
             this.Controls.Add(this.CourseDataGrid);
             this.Controls.Add(this.TermSelectorBox);
             this.Controls.Add(this.MenuReturn);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "stuCourseHist";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Course History";
