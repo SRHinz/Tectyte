435cc9d8fc4ebfe62a84708ee7de57356b4a738c
misha-matchette <mgmatchette@coe.edu>
2020-10-09 17:42:44 -0500

    removed dropCourses form, added view advisees button to form

--- RegSystemGUI/Options.Designer.cs
+++ RegSystemGUI/Options.Designer.cs
@@ -33,11 +33,12 @@
             this.CourseHisButton = new System.Windows.Forms.Button();
             this.logOutButton = new System.Windows.Forms.Button();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.viewAdvisees = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // ViewCoursesButton
             // 
-            this.ViewCoursesButton.Location = new System.Drawing.Point(221, 181);
+            this.ViewCoursesButton.Location = new System.Drawing.Point(214, 181);
             this.ViewCoursesButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.ViewCoursesButton.Name = "ViewCoursesButton";
             this.ViewCoursesButton.Size = new System.Drawing.Size(202, 73);
@@ -48,10 +49,10 @@
             // 
             // CourseHisButton
             // 
-            this.CourseHisButton.Location = new System.Drawing.Point(505, 181);
+            this.CourseHisButton.Location = new System.Drawing.Point(486, 181);
             this.CourseHisButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.CourseHisButton.Name = "CourseHisButton";
-            this.CourseHisButton.Size = new System.Drawing.Size(177, 73);
+            this.CourseHisButton.Size = new System.Drawing.Size(202, 73);
             this.CourseHisButton.TabIndex = 1;
             this.CourseHisButton.Text = "View Course History";
             this.CourseHisButton.UseVisualStyleBackColor = true;
@@ -59,7 +60,7 @@
             // 
             // logOutButton
             // 
-            this.logOutButton.Location = new System.Drawing.Point(363, 327);
+            this.logOutButton.Location = new System.Drawing.Point(356, 327);
             this.logOutButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.logOutButton.Name = "logOutButton";
             this.logOutButton.Size = new System.Drawing.Size(177, 73);
@@ -78,18 +79,29 @@
             this.menuStrip1.TabIndex = 3;
             this.menuStrip1.Text = "menuStrip1";
             // 
+            // viewAdvisees
+            // 
+            this.viewAdvisees.Location = new System.Drawing.Point(486, 181);
+            this.viewAdvisees.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.viewAdvisees.Name = "viewAdvisees";
+            this.viewAdvisees.Size = new System.Drawing.Size(202, 73);
+            this.viewAdvisees.TabIndex = 4;
+            this.viewAdvisees.Text = "View Advisees";
+            this.viewAdvisees.UseVisualStyleBackColor = true;
+            // 
             // Options
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(902, 581);
+            this.Controls.Add(this.viewAdvisees);
             this.Controls.Add(this.logOutButton);
             this.Controls.Add(this.CourseHisButton);
             this.Controls.Add(this.ViewCoursesButton);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Options";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menu";
@@ -105,5 +117,6 @@
         private System.Windows.Forms.Button CourseHisButton;
         private System.Windows.Forms.Button logOutButton;
         private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Button viewAdvisees;
     }
 }
