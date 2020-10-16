712cb337d9de64cd83bdb9ff03ec2ec3794660e5
misha-matchette <mgmatchette@coe.edu>
2020-10-16 15:11:53 -0500

    added buttons to students enrolled to return to menu and return to the courseviewer

--- RegSystemGUI/studentsEnrolled.Designer.cs
+++ RegSystemGUI/studentsEnrolled.Designer.cs
@@ -32,13 +32,15 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.copyrightBox = new System.Windows.Forms.TextBox();
             this.listView1 = new System.Windows.Forms.ListView();
+            this.MenuReturn = new System.Windows.Forms.Button();
+            this.courseReturn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(201, 829);
+            this.pictureBox1.Location = new System.Drawing.Point(330, 793);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(38, 38);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -49,7 +51,7 @@
             // 
             this.copyrightBox.BackColor = System.Drawing.SystemColors.Control;
             this.copyrightBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.copyrightBox.Location = new System.Drawing.Point(231, 836);
+            this.copyrightBox.Location = new System.Drawing.Point(360, 800);
             this.copyrightBox.Name = "copyrightBox";
             this.copyrightBox.Size = new System.Drawing.Size(416, 24);
             this.copyrightBox.TabIndex = 8;
@@ -59,7 +61,7 @@
             // listView1
             // 
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(157, 97);
+            this.listView1.Location = new System.Drawing.Point(50, 45);
             this.listView1.Margin = new System.Windows.Forms.Padding(5);
             this.listView1.MultiSelect = false;
             this.listView1.Name = "listView1";
@@ -68,11 +70,33 @@
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.List;
             // 
+            // MenuReturn
+            // 
+            this.MenuReturn.Location = new System.Drawing.Point(649, 257);
+            this.MenuReturn.Name = "MenuReturn";
+            this.MenuReturn.Size = new System.Drawing.Size(406, 73);
+            this.MenuReturn.TabIndex = 12;
+            this.MenuReturn.Text = "Return to Menu";
+            this.MenuReturn.UseVisualStyleBackColor = true;
+            this.MenuReturn.Click += new System.EventHandler(this.MenuReturn_Click);
+            // 
+            // courseReturn
+            // 
+            this.courseReturn.Location = new System.Drawing.Point(649, 420);
+            this.courseReturn.Name = "courseReturn";
+            this.courseReturn.Size = new System.Drawing.Size(406, 73);
+            this.courseReturn.TabIndex = 13;
+            this.courseReturn.Text = "Return to My Courses";
+            this.courseReturn.UseVisualStyleBackColor = true;
+            this.courseReturn.Click += new System.EventHandler(this.courseReturn_Click);
+            // 
             // studentsEnrolled
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(848, 872);
+            this.ClientSize = new System.Drawing.Size(1107, 836);
+            this.Controls.Add(this.courseReturn);
+            this.Controls.Add(this.MenuReturn);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.copyrightBox);
@@ -91,5 +115,7 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TextBox copyrightBox;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button MenuReturn;
+        private System.Windows.Forms.Button courseReturn;
     }
 }
