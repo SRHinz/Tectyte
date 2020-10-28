b5796fbbd3ccbaf6dd66ca2ca649c92709271564
brkolpin <brkolpin@coe.edu>
2020-10-28 14:46:47 -0500

    Created accountSelector Form

---
+++ RegSystemGUI/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace RegSystemGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AccountDataGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.AccountDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AccountDataGrid
+            // 
+            this.AccountDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.AccountDataGrid.Location = new System.Drawing.Point(64, 32);
+            this.AccountDataGrid.Name = "AccountDataGrid";
+            this.AccountDataGrid.RowHeadersWidth = 51;
+            this.AccountDataGrid.RowTemplate.Height = 24;
+            this.AccountDataGrid.Size = new System.Drawing.Size(240, 150);
+            this.AccountDataGrid.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.AccountDataGrid);
+            this.Name = "Form1";
+            this.Text = "Account Selector";
+            ((System.ComponentModel.ISupportInitialize)(this.AccountDataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView AccountDataGrid;
+    }
+}
