b01462189ea3c5af4fd1a51c7b98ecb866b6c75c
brkolpin <brkolpin@coe.edu>
2020-10-28 14:51:40 -0500

    Added Search bar

--- RegSystemGUI/Form1.Designer.cs
+++ RegSystemGUI/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RegSystemGUI
 {
-    partial class Form1
+    partial class AccountSelector
     {
         /// <summary>
         /// Required designer variable.
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.AccountDataGrid = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SearchLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.AccountDataGrid)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,21 +44,43 @@
             this.AccountDataGrid.Size = new System.Drawing.Size(240, 150);
             this.AccountDataGrid.TabIndex = 0;
             // 
-            // Form1
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(527, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(151, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // SearchLabel
+            // 
+            this.SearchLabel.AutoSize = true;
+            this.SearchLabel.Location = new System.Drawing.Point(452, 35);
+            this.SearchLabel.Name = "SearchLabel";
+            this.SearchLabel.Size = new System.Drawing.Size(57, 17);
+            this.SearchLabel.TabIndex = 2;
+            this.SearchLabel.Text = "Search:";
+            // 
+            // AccountSelector
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SearchLabel);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.AccountDataGrid);
-            this.Name = "Form1";
+            this.Name = "AccountSelector";
             this.Text = "Account Selector";
             ((System.ComponentModel.ISupportInitialize)(this.AccountDataGrid)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView AccountDataGrid;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label SearchLabel;
     }
 }
