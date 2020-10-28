31fee7a419d084b754d67fe04f2bc32c61dd8ae0
brkolpin <brkolpin@coe.edu>
2020-10-28 14:55:18 -0500

    Merge Commit

--- RegSystemGUI/Form1.Designer.cs
+++ RegSystemGUI/Form1.Designer.cs
@@ -31,22 +31,29 @@
             this.AccountDataGrid = new System.Windows.Forms.DataGridView();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.SearchLabel = new System.Windows.Forms.Label();
+            this.LastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Username = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.AccountDataGrid)).BeginInit();
             this.SuspendLayout();
             // 
             // AccountDataGrid
             // 
             this.AccountDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.AccountDataGrid.Location = new System.Drawing.Point(64, 32);
+            this.AccountDataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.LastName,
+            this.FirstName,
+            this.Username});
+            this.AccountDataGrid.Location = new System.Drawing.Point(12, 12);
             this.AccountDataGrid.Name = "AccountDataGrid";
             this.AccountDataGrid.RowHeadersWidth = 51;
             this.AccountDataGrid.RowTemplate.Height = 24;
-            this.AccountDataGrid.Size = new System.Drawing.Size(240, 150);
+            this.AccountDataGrid.Size = new System.Drawing.Size(435, 394);
             this.AccountDataGrid.TabIndex = 0;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(527, 32);
+            this.textBox1.Location = new System.Drawing.Point(648, 32);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(151, 22);
             this.textBox1.TabIndex = 1;
@@ -55,11 +62,32 @@
             // SearchLabel
             // 
             this.SearchLabel.AutoSize = true;
-            this.SearchLabel.Location = new System.Drawing.Point(452, 35);
+            this.SearchLabel.Location = new System.Drawing.Point(585, 35);
             this.SearchLabel.Name = "SearchLabel";
             this.SearchLabel.Size = new System.Drawing.Size(57, 17);
             this.SearchLabel.TabIndex = 2;
             this.SearchLabel.Text = "Search:";
+            // 
+            // LastName
+            // 
+            this.LastName.HeaderText = "Last Name";
+            this.LastName.MinimumWidth = 6;
+            this.LastName.Name = "LastName";
+            this.LastName.Width = 125;
+            // 
+            // FirstName
+            // 
+            this.FirstName.HeaderText = "First Name";
+            this.FirstName.MinimumWidth = 6;
+            this.FirstName.Name = "FirstName";
+            this.FirstName.Width = 125;
+            // 
+            // Username
+            // 
+            this.Username.HeaderText = "Username";
+            this.Username.MinimumWidth = 6;
+            this.Username.Name = "Username";
+            this.Username.Width = 125;
             // 
             // AccountSelector
             // 
@@ -82,5 +110,8 @@
         private System.Windows.Forms.DataGridView AccountDataGrid;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label SearchLabel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LastName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FirstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Username;
     }
 }
