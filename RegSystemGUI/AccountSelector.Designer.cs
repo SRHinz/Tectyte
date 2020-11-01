38096e2bfb6844c3c4be9a9298524e14c98b4989
misha-matchette <mgmatchette@coe.edu>
2020-11-01 11:42:29 -0600

    account Selector GUI standardized

--- RegSystemGUI/AccountSelector.Designer.cs
+++ RegSystemGUI/AccountSelector.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AccountSelector));
             this.AccountDataGrid = new System.Windows.Forms.DataGridView();
             this.LastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.FirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -35,7 +36,10 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.SearchLabel = new System.Windows.Forms.Label();
             this.SelectButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.copyrightBox = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.AccountDataGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // AccountDataGrid
@@ -49,13 +53,14 @@
             this.LastName,
             this.FirstName,
             this.Username});
-            this.AccountDataGrid.Location = new System.Drawing.Point(12, 12);
+            this.AccountDataGrid.Location = new System.Drawing.Point(19, 19);
+            this.AccountDataGrid.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.AccountDataGrid.Name = "AccountDataGrid";
             this.AccountDataGrid.ReadOnly = true;
             this.AccountDataGrid.RowHeadersWidth = 51;
             this.AccountDataGrid.RowTemplate.Height = 24;
             this.AccountDataGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.AccountDataGrid.Size = new System.Drawing.Size(435, 394);
+            this.AccountDataGrid.Size = new System.Drawing.Size(696, 630);
             this.AccountDataGrid.TabIndex = 0;
             this.AccountDataGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.AccountDataGrid_CellContentClick);
             // 
@@ -65,7 +70,7 @@
             this.LastName.MinimumWidth = 6;
             this.LastName.Name = "LastName";
             this.LastName.ReadOnly = true;
-            this.LastName.Width = 105;
+            this.LastName.Width = 160;
             // 
             // FirstName
             // 
@@ -73,7 +78,7 @@
             this.FirstName.MinimumWidth = 6;
             this.FirstName.Name = "FirstName";
             this.FirstName.ReadOnly = true;
-            this.FirstName.Width = 105;
+            this.FirstName.Width = 161;
             // 
             // Username
             // 
@@ -81,49 +86,81 @@
             this.Username.MinimumWidth = 6;
             this.Username.Name = "Username";
             this.Username.ReadOnly = true;
-            this.Username.Width = 102;
+            this.Username.Width = 155;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(648, 32);
+            this.textBox1.Location = new System.Drawing.Point(1037, 51);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(151, 22);
+            this.textBox1.Size = new System.Drawing.Size(239, 31);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // SearchLabel
             // 
             this.SearchLabel.AutoSize = true;
-            this.SearchLabel.Location = new System.Drawing.Point(494, 35);
+            this.SearchLabel.Location = new System.Drawing.Point(790, 56);
+            this.SearchLabel.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.SearchLabel.Name = "SearchLabel";
-            this.SearchLabel.Size = new System.Drawing.Size(148, 17);
+            this.SearchLabel.Size = new System.Drawing.Size(224, 25);
             this.SearchLabel.TabIndex = 2;
             this.SearchLabel.Text = "Search by Last Name:";
             // 
             // SelectButton
             // 
-            this.SelectButton.Location = new System.Drawing.Point(684, 95);
+            this.SelectButton.Location = new System.Drawing.Point(1094, 152);
+            this.SelectButton.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.SelectButton.Name = "SelectButton";
-            this.SelectButton.Size = new System.Drawing.Size(96, 37);
+            this.SelectButton.Size = new System.Drawing.Size(154, 59);
             this.SelectButton.TabIndex = 3;
             this.SelectButton.Text = "Select";
             this.SelectButton.UseVisualStyleBackColor = true;
             this.SelectButton.Click += new System.EventHandler(this.SelectButton_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(419, 808);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // copyrightBox
+            // 
+            this.copyrightBox.BackColor = System.Drawing.SystemColors.Control;
+            this.copyrightBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.copyrightBox.Location = new System.Drawing.Point(451, 816);
+            this.copyrightBox.Margin = new System.Windows.Forms.Padding(5);
+            this.copyrightBox.Name = "copyrightBox";
+            this.copyrightBox.Size = new System.Drawing.Size(443, 24);
+            this.copyrightBox.TabIndex = 14;
+            this.copyrightBox.Text = "this software is property of tectyte ©2020";
+            this.copyrightBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
             // AccountSelector
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(192F, 192F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(988, 534);
+            this.ClientSize = new System.Drawing.Size(1313, 854);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.copyrightBox);
             this.Controls.Add(this.SelectButton);
             this.Controls.Add(this.SearchLabel);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.AccountDataGrid);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "AccountSelector";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Account Selector";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.AccountSelector_Load);
             ((System.ComponentModel.ISupportInitialize)(this.AccountDataGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -138,5 +175,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn FirstName;
         private System.Windows.Forms.DataGridViewTextBoxColumn Username;
         private System.Windows.Forms.Button SelectButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox copyrightBox;
     }
 }
