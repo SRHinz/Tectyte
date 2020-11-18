5638914f55283312f74d26e20461789517d5ca9c
misha-matchette <mgmatchette@coe.edu>
2020-11-17 23:49:00 -0600

    deleted some obselete code

--- RegSystemGUI/CourseSelector.cs
+++ RegSystemGUI/CourseSelector.cs
@@ -56,10 +56,6 @@ namespace RegSystemGUI
             }
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
 
         private void DelCourseClick(object sender, EventArgs e)
         {
