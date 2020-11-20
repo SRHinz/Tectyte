528a9774b56161795ad5a417a4e4a1dc96e5b463
brkolpin <brkolpin@coe.edu>
2020-11-19 22:37:33 -0600

    fixed headings on students enrolled form to match the data

--- RegSystemGUI/studentsEnrolled.Designer.cs
+++ RegSystemGUI/studentsEnrolled.Designer.cs
@@ -37,9 +37,9 @@
             this.label1 = new System.Windows.Forms.Label();
             this.courseBox = new System.Windows.Forms.TextBox();
             this.enrolledStudentDatagrid = new System.Windows.Forms.DataGridView();
-            this.LastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.FirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Username = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.enrolledStudentDatagrid)).BeginInit();
             this.SuspendLayout();
@@ -47,9 +47,10 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(330, 793);
+            this.pictureBox1.Location = new System.Drawing.Point(220, 508);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(38, 38);
+            this.pictureBox1.Size = new System.Drawing.Size(25, 24);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
@@ -58,9 +59,10 @@
             // 
             this.copyrightBox.BackColor = System.Drawing.SystemColors.Control;
             this.copyrightBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.copyrightBox.Location = new System.Drawing.Point(360, 800);
+            this.copyrightBox.Location = new System.Drawing.Point(240, 512);
+            this.copyrightBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.copyrightBox.Name = "copyrightBox";
-            this.copyrightBox.Size = new System.Drawing.Size(416, 24);
+            this.copyrightBox.Size = new System.Drawing.Size(277, 15);
             this.copyrightBox.TabIndex = 8;
             this.copyrightBox.TabStop = false;
             this.copyrightBox.Text = "this software is property of tectyte ©2020";
@@ -68,9 +70,10 @@
             // 
             // MenuReturn
             // 
-            this.MenuReturn.Location = new System.Drawing.Point(649, 257);
+            this.MenuReturn.Location = new System.Drawing.Point(433, 164);
+            this.MenuReturn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MenuReturn.Name = "MenuReturn";
-            this.MenuReturn.Size = new System.Drawing.Size(406, 73);
+            this.MenuReturn.Size = new System.Drawing.Size(271, 47);
             this.MenuReturn.TabIndex = 0;
             this.MenuReturn.Text = "Return to Menu";
             this.MenuReturn.UseVisualStyleBackColor = true;
@@ -78,9 +81,10 @@
             // 
             // courseReturn
             // 
-            this.courseReturn.Location = new System.Drawing.Point(649, 420);
+            this.courseReturn.Location = new System.Drawing.Point(433, 269);
+            this.courseReturn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.courseReturn.Name = "courseReturn";
-            this.courseReturn.Size = new System.Drawing.Size(406, 73);
+            this.courseReturn.Size = new System.Drawing.Size(271, 47);
             this.courseReturn.TabIndex = 1;
             this.courseReturn.Text = "Return to My Courses";
             this.courseReturn.UseVisualStyleBackColor = true;
@@ -90,9 +94,10 @@
             // 
             this.noEnrolledBox.BackColor = System.Drawing.SystemColors.Control;
             this.noEnrolledBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.noEnrolledBox.Location = new System.Drawing.Point(50, 12);
+            this.noEnrolledBox.Location = new System.Drawing.Point(33, 8);
+            this.noEnrolledBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.noEnrolledBox.Name = "noEnrolledBox";
-            this.noEnrolledBox.Size = new System.Drawing.Size(539, 24);
+            this.noEnrolledBox.Size = new System.Drawing.Size(359, 15);
             this.noEnrolledBox.TabIndex = 14;
             this.noEnrolledBox.Text = "**No students are currently registered for this course.**";
             // 
@@ -100,9 +105,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(751, 12);
+            this.label1.Location = new System.Drawing.Point(501, 8);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(117, 31);
+            this.label1.Size = new System.Drawing.Size(73, 20);
             this.label1.TabIndex = 15;
             this.label1.Text = "Course: ";
             // 
@@ -111,9 +117,10 @@
             this.courseBox.BackColor = System.Drawing.SystemColors.Control;
             this.courseBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.courseBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.courseBox.Location = new System.Drawing.Point(874, 12);
+            this.courseBox.Location = new System.Drawing.Point(583, 8);
+            this.courseBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.courseBox.Name = "courseBox";
-            this.courseBox.Size = new System.Drawing.Size(221, 31);
+            this.courseBox.Size = new System.Drawing.Size(147, 20);
             this.courseBox.TabIndex = 16;
             this.courseBox.Text = "placeholder";
             // 
@@ -126,32 +133,17 @@
             this.enrolledStudentDatagrid.BackgroundColor = System.Drawing.SystemColors.Control;
             this.enrolledStudentDatagrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.enrolledStudentDatagrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.LastName,
+            this.Username,
             this.FirstName,
-            this.Username});
-            this.enrolledStudentDatagrid.Location = new System.Drawing.Point(50, 44);
-            this.enrolledStudentDatagrid.Margin = new System.Windows.Forms.Padding(5);
+            this.LastName});
+            this.enrolledStudentDatagrid.Location = new System.Drawing.Point(33, 28);
             this.enrolledStudentDatagrid.Name = "enrolledStudentDatagrid";
             this.enrolledStudentDatagrid.ReadOnly = true;
             this.enrolledStudentDatagrid.RowHeadersWidth = 4;
             this.enrolledStudentDatagrid.RowTemplate.Height = 24;
             this.enrolledStudentDatagrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.enrolledStudentDatagrid.Size = new System.Drawing.Size(539, 679);
+            this.enrolledStudentDatagrid.Size = new System.Drawing.Size(359, 435);
             this.enrolledStudentDatagrid.TabIndex = 24;
-            // 
-            // LastName
-            // 
-            this.LastName.HeaderText = "Last Name";
-            this.LastName.MinimumWidth = 6;
-            this.LastName.Name = "LastName";
-            this.LastName.ReadOnly = true;
-            // 
-            // FirstName
-            // 
-            this.FirstName.HeaderText = "First Name";
-            this.FirstName.MinimumWidth = 6;
-            this.FirstName.Name = "FirstName";
-            this.FirstName.ReadOnly = true;
             // 
             // Username
             // 
@@ -160,11 +152,25 @@
             this.Username.Name = "Username";
             this.Username.ReadOnly = true;
             // 
+            // FirstName
+            // 
+            this.FirstName.HeaderText = "First Name";
+            this.FirstName.MinimumWidth = 6;
+            this.FirstName.Name = "FirstName";
+            this.FirstName.ReadOnly = true;
+            // 
+            // LastName
+            // 
+            this.LastName.HeaderText = "Last Name";
+            this.LastName.MinimumWidth = 6;
+            this.LastName.Name = "LastName";
+            this.LastName.ReadOnly = true;
+            // 
             // studentsEnrolled
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1107, 836);
+            this.ClientSize = new System.Drawing.Size(738, 535);
             this.Controls.Add(this.enrolledStudentDatagrid);
             this.Controls.Add(this.courseBox);
             this.Controls.Add(this.label1);
@@ -174,6 +180,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.copyrightBox);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "studentsEnrolled";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Students Enrolled";
@@ -195,8 +202,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox courseBox;
         private System.Windows.Forms.DataGridView enrolledStudentDatagrid;
-        private System.Windows.Forms.DataGridViewTextBoxColumn LastName;
-        private System.Windows.Forms.DataGridViewTextBoxColumn FirstName;
         private System.Windows.Forms.DataGridViewTextBoxColumn Username;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FirstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LastName;
     }
 }
